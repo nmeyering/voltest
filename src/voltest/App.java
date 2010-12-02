8faf4b5aabd730a505940c13fbd6139c0da9651a
Niels Meyering <nmeyering@gmail.com>
2010-12-02 16:44:41 +0100

    unfinished changes to view

--- src/voltest/App.java
+++ src/voltest/App.java
@@ -1,5 +1,11 @@
 package voltest;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
 import javax.swing.JFrame;
 
 public class App
@@ -12,5 +18,56 @@ public class App
 		
 		frame.add( panel );
 		frame.setVisible( true );
+	}
+	
+	public static void draw()
+	{
+		Graphics g = new Graphics2D();
+		Dimension size = getSize();
+		Box box = new Box(
+				new Vector(
+						-5,
+						-5,
+						-50
+						),
+						10
+						);
+		Context context = new Context(
+				new Rectangle(
+						0,
+						0,
+						size.width,
+						size.height
+						),
+				Context.perspectiveMatrix(
+						Math.toRadians( 60 ),
+						(double) size.width / size.height,
+						1,
+						100)
+				);
+			g2d.setColor( Color.BLUE );
+			g2d.drawLine(0, 0, 320, 240);
+			g2d.drawLine(320, 0, 0, 240);
+			g2d.setColor( Color.WHITE );
+			g2d.setXORMode( Color.BLACK );
+		
+			for( int i = 0; i < size.height; ++i )
+			{
+				System.out.println("Rendering line " + i );
+				for( int j = 0; j < size.width; ++j )
+				{
+					if(	Ray.intersect(
+									new Vector(
+											j,
+											i,
+											0
+									),
+									box,
+									context
+							)
+						)
+						setPixel(i, j);
+				}
+			}		
 	}
 }
--- src/voltest/ViewPanel.java
+++ src/voltest/ViewPanel.java
@@ -7,67 +7,34 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
 
 public class ViewPanel extends JPanel
 {
-	public void paintComponent(
-			Graphics g
-			)
+	private WritableRaster data;
+	
+	public void paint( Graphics g )
 	{
-		super.paintComponents( g );
-		Graphics2D g2d = (Graphics2D) g;
-//		Dimension size = getSize();
-		Dimension size = new Dimension( 320, 240 );
-		Box box = new Box(
-				new Vector(
-						-5,
-						-5,
-						-50
+		g.drawImage(
+				new BufferedImage(
+						,
+						data,
+						false,
+						null
 						),
-						10
-						);
-		Context context = new Context(
-				new Rectangle(
-						0,
-						0,
-						size.width,
-						size.height
-						),
-				Context.perspectiveMatrix(
-						Math.toRadians( 60 ),
-						(double) size.width / size.height,
-						1,
-						100)
-				);
-			g2d.setColor( Color.BLUE );
-			g2d.drawLine(0, 0, 320, 240);
-			g2d.drawLine(320, 0, 0, 240);
-			g2d.setColor( Color.WHITE );
-			g2d.setXORMode( Color.BLACK );
-		
-			for( int i = 0; i < size.height; ++i )
-			{
-				System.out.println("Rendering line " + i );
-				for( int j = 0; j < size.width; ++j )
-				{
-					if(	Ray.intersect(
-									new Vector(
-											j,
-											i,
-											0
-									),
-									box,
-									context
-							)
-						)
-						g2d.drawLine(
-								j,
-								i,
-								j,
-								i
-								);
-				}
-			}
-		
+				0,
+				0,
+				null);
+	}
+	
+	public void setPixel( int x, int y )
+	{
+		data.setPixel(
+				x,
+				y,
+				new int[1]
+				        );
 	}
 }
