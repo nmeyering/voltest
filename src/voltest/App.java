0dd6608fdd19b9d6fda406274adf3b72e7599a56
Niels Meyering <nmeyering@gmail.com>
2010-12-04 14:16:24 +0100

    faster drawing code

--- src/voltest/App.java
+++ src/voltest/App.java
@@ -5,26 +5,31 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.image.DataBufferByte;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
 
 import javax.swing.JFrame;
 
 public class App
 {
+	static ViewPanel panel;
+	static Context context;
+	static Box box;
+	static byte[][] CLEAR_DATA = new byte[320][240];
+	
 	public static void main( String[] args )
 	{
-		JFrame frame = new JFrame();
-		ViewPanel panel = new ViewPanel();
-		panel.setSize( 320, 240 );
+		Dimension size = new Dimension(
+				320,
+				240
+				);
 		
-		frame.add( panel );
-		frame.setVisible( true );
-	}
-	
-	public static void draw()
-	{
-		Graphics g = new Graphics2D();
-		Dimension size = getSize();
-		Box box = new Box(
+		JFrame frame = new JFrame();
+		panel = new ViewPanel();
+		panel.setSize( size );
+		
+		box = new Box(
 				new Vector(
 						-5,
 						-5,
@@ -32,7 +37,7 @@ public class App
 						),
 						10
 						);
-		Context context = new Context(
+		context = new Context(
 				new Rectangle(
 						0,
 						0,
@@ -45,29 +50,37 @@ public class App
 						1,
 						100)
 				);
-			g2d.setColor( Color.BLUE );
-			g2d.drawLine(0, 0, 320, 240);
-			g2d.drawLine(320, 0, 0, 240);
-			g2d.setColor( Color.WHITE );
-			g2d.setXORMode( Color.BLACK );
 		
-			for( int i = 0; i < size.height; ++i )
-			{
-				System.out.println("Rendering line " + i );
-				for( int j = 0; j < size.width; ++j )
-				{
-					if(	Ray.intersect(
-									new Vector(
-											j,
-											i,
-											0
-									),
-									box,
-									context
-							)
-						)
-						setPixel(i, j);
-				}
-			}		
+		frame.add( panel );
+		frame.setVisible( true );
+		draw();
 	}
+	
+
+	public static void draw()
+	{
+		if(box.src.z < 0)
+			box.src.z++;
+		panel.data = new DataBufferByte(
+				CLEAR_DATA,
+				320*240
+				);
+		for( int i = 0; i < 240; ++i )
+		{
+			for( int j = 0; j < 320; ++j )
+			{
+				if(	Ray.intersect(
+								new Vector(
+										i,
+										j,
+										0
+								),
+								box,
+								context
+						)
+					)
+					panel.setPixel(i, j);
+			}
+		}		
+}
 }
--- src/voltest/ViewPanel.java
+++ src/voltest/ViewPanel.java
@@ -6,24 +6,50 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.ComponentSampleModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
 import java.awt.image.Raster;
+import java.awt.image.SampleModel;
+import java.awt.image.SinglePixelPackedSampleModel;
 import java.awt.image.WritableRaster;
 
 public class ViewPanel extends JPanel
 {
-	private WritableRaster data;
+	SampleModel sm = new SinglePixelPackedSampleModel(
+			DataBuffer.TYPE_BYTE,
+			320,
+			240,
+			new int[] {0xFF}
+			);
+	DataBuffer data =
+		new DataBufferByte(
+				320*240
+				);
+	WritableRaster raster =
+		Raster.createWritableRaster(
+				sm,
+				data,
+				null
+				);
+	private int[] SET = {0xFF};
 	
 	public void paint( Graphics g )
 	{
+		BufferedImage img = new BufferedImage(
+				320,
+				240,
+				BufferedImage.TYPE_BYTE_GRAY
+				);
+		App.draw();
+		img.setData( raster );
 		g.drawImage(
-				new BufferedImage(
-						,
-						data,
-						false,
-						null
-						),
+				img,
 				0,
 				0,
 				null);
@@ -31,10 +57,10 @@ public class ViewPanel extends JPanel
 	
 	public void setPixel( int x, int y )
 	{
-		data.setPixel(
+		raster.setPixel(
 				x,
 				y,
-				new int[1]
-				        );
-	}
+				SET
+				);
+	}	
 }
